f69a08932bb1305d10b8ead707320df50d4ddbb4
5llo <mhmdkhalil822@gmail.com>
2023-06-21 12:53:58 +0300

    not complete

---
+++ database/migrations/2023_06_21_070559_create_order_product_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_product', function (Blueprint $table) {
+
+            $table->id();
+            $table->foreignId('order_id')->constrained()->ondelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->unique(['order_id','product_id']);
+            $table->integer('quantity');
+            $table->timestamps();
+
+
+
+
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_product');
+    }
+};
